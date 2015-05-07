f861f0c2d354a1a0ad9149d0432c7781ed83e16e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-05-06 22:57:18 -0300

    fish(functions.fuck): remove fucked up command from history

--- fish/functions/fuck.fish
+++ fish/functions/fuck.fish
@@ -4,8 +4,13 @@ function __thefuck_repl -d 'Replace operators into fish-compatible'
 end
 
 function fuck -d 'Correct your previous console command'
+    set -l exit_code $status
     set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
-    thefuck $history[1] > $eval_script
+    set -l fucked_up_commandd $history[1]
+    thefuck $fucked_up_commandd > $eval_script
     eval (__thefuck_repl (cat $eval_script))
     rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_commandd
+    end
 end
