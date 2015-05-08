e2d24d49f6600a609e82d5ea726163c36f695792
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-05-08 11:57:53 -0300

    fish(fuck): adapt function to upcoming The Fuck release

--- fish/functions/fuck.fish
+++ fish/functions/fuck.fish
@@ -1,14 +1,9 @@
-function __thefuck_repl -d 'Replace operators into fish-compatible'
-    set -l tmp (echo $argv | sed 's/ && / ; and /g')
-    echo $tmp | sed 's/ || / ; or /g'
-end
-
 function fuck -d 'Correct your previous console command'
     set -l exit_code $status
     set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
     set -l fucked_up_commandd $history[1]
     thefuck $fucked_up_commandd > $eval_script
-    eval (__thefuck_repl (cat $eval_script))
+    . $eval_script
     rm $eval_script
     if test $exit_code -ne 0
         history --delete $fucked_up_commandd
