ebfd72cfcf91f10cd926c2c0b6d85509be103b46
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-05-25 17:28:37 -0300

    fish(function): refactor lss function to support GNU/Linux
    Signed-off-by: Pablo Santiago Blum de Aguiar <scorphus@gmail.com>

--- fish/functions/lss.fish
+++ fish/functions/lss.fish
@@ -1,3 +1,7 @@
 function lss --description "Show more detailed 'ls' output"
-    gls -l --all --human-readable --classify --color=always $argv | less -XRF
+    if [ (uname) = "Darwin" ]
+        gls -lah --classify --color=always $argv | less -XRF
+    else
+        ls -lah --classify --color=always $argv | less -XRF
+    end
 end
