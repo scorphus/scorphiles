e3df06a532c12cee1371e37b39c0dba337a1337b
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-06-07 23:49:40 -0300

    fish(functions): change `fuck` to set the TF_ALIAS var

--- fish/functions/fuck.fish
+++ fish/functions/fuck.fish
@@ -1,5 +1,6 @@
 function fuck -d 'Correct your previous console command'
     set -l exit_code $status
+    set -l TF_ALIAS fuck
     set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
     set -l fucked_up_commandd $history[1]
     thefuck $fucked_up_commandd > $eval_script
