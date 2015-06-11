b4d021fe0d659d407659db5b7459a483c8caeca9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-06-11 17:51:59 -0300

    improve(helpers.django): add nosetests helper functions

--- fish/helpers/django.fish
+++ fish/helpers/django.fish
@@ -31,3 +31,42 @@ function run_test_server_no_reload -d 'make -n clean_pycs run_test_server'
     python -u tests/testproject/manage.py runserver 0.0.0.0:8000 \
         --settings=testproject.settings_IGNOREME --noreload
 end
+
+function nosetests-unit -a verbosity -d 'Run nosetests unit tests'
+    clean-pycs
+    set -x REUSE_DB "1"
+    coverage run tests/testproject/manage.py test tests/ \
+        --settings=testproject.settings_test \
+        --with-yanc --yanc-color=yes $verbosity
+    if test $status -eq 0
+        coverage report -m --fail-under=65
+    end
+end
+
+function nosetests-unit-v -d 'Run nosetests unit tests verbosely'
+    nosetests-unit --verbosity=2
+end
+
+function nosetests-focus -a verbosity -d 'Run nosetests focused tests'
+    clean-pycs
+    set -x REUSE_DB "1"
+    coverage run tests/testproject/manage.py test tests/ \
+        --settings=testproject.settings_test_focus \
+        --with-yanc --yanc-color=yes --ipdb --ipdb-failures $verbosity
+end
+
+function nosetests-focus-v -d 'Run nosetests focused tests verbosely'
+    nosetests-focus --verbosity=2
+end
+
+function nosetests-focus-ignore -a verbosity -d 'Run nosetests ignoring focus-ignored tests'
+    clean-pycs
+    set -x REUSE_DB "1"
+    coverage run tests/testproject/manage.py test tests/ \
+        --settings=testproject.settings_test_focus_ignore \
+        --with-yanc --yanc-color=yes $verbosity
+end
+
+function nosetests-focus_ignore-v -d 'Run nosetests ignoring focus-ignored tests verbosely'
+    nosetests-ignore --verbosity=2
+end
