5591330d7014b9ddb3ef2deba7c464e6e5d11ad1
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-06-22 19:49:24 -0300

    improve(fish): allow using gpip with more than one argument
    For example: `gpip -r requirements.txt`

--- fish/functions/gpip.fish
+++ fish/functions/gpip.fish
@@ -1,7 +1,6 @@
 function gpip -d "Globoi's artifactory-aware pip"
-    if test (count $argv) -eq 2
+    if test (count $argv) -ge 2
         if test $argv[1] = 'install'
-            echo $argv[2]
             pip install \
                 --allow-external \
                 --allow-unverified \
@@ -9,13 +8,13 @@ function gpip -d "Globoi's artifactory-aware pip"
                 --index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi/simple \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi/simple/ \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi-all/simple/ \
-                -U $argv[2]
+                -U $argv[2..-1]
         else
             echo 'Sorry, we only support `install`, mate :('
             return 1
         end
     else
-        echo 'Too many arguments, dude :|'
+        echo 'Wrong arguments number, dude :|'
         return 1
     end
 end
