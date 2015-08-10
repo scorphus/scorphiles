fbe9fbcc332b2f35e261ecf652adddc295b57869
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-09 22:52:35 -0300

    fix(completions.nosetests): use tr instead of sed

--- fish/completions/nosetests.fish
+++ fish/completions/nosetests.fish
@@ -4,7 +4,7 @@
 
 function __fish_nosetests
     set -l file (commandline -ot)
-    command nosecomplete $file ^/dev/null | sed -e 's/ /\n/g'
+    command nosecomplete $file ^/dev/null | tr ' ' '\n'
 end
 
 complete -f -c nosetests -a '(__fish_nosetests)' -d 'Nosetests'
