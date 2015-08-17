987a86e095928ac167584c5c057f15bb329be0bc
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-17 13:43:46 -0300

    refact(fish.functions.fuck): amend fuck to make it work

--- fish/functions/fuck.fish
+++ fish/functions/fuck.fish
@@ -1,12 +1,14 @@
 function fuck -d 'Correct your previous console command'
-    set -l TF_ALIAS fuck
     set -l exit_code $status
+    set -x TF_ALIAS fuck
     set -l fucked_up_command $history[1]
-    thefuck $fucked_up_command | read unfucked
-    eval $unfucked
-    if test $exit_code -ne 0
-        history --delete $fucked_up_command
-        history --merge
-        return 0
+    thefuck $fucked_up_command | read unfucked_command
+    if [ "$unfucked_command" != "" ]
+        eval $unfucked_command
+        if test $exit_code -ne 0
+            history --delete $fucked_up_command
+            history --merge
+            return 0
+        end
     end
 end
