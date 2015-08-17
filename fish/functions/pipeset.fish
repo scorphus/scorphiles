413ee9c44dc76d712c833cb0e3f1a83a77310972
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-16 23:53:49 -0300

    fish(cleanup): remove an unnecessary function and clean pipeset up

--- fish/functions/pipeset.fish
+++ fish/functions/pipeset.fish
@@ -3,7 +3,6 @@ function pipeset --no-scope-shadowing -d "Set multiline variable"
     set -l _options
     set -l _variables
     for _item in $argv
-        echo 1 $_item
         switch $_item
             case '-*'
                 set _options $_options $_item
@@ -12,14 +11,10 @@ function pipeset --no-scope-shadowing -d "Set multiline variable"
         end
     end
     for _variable in $_variables
-        echo 2 $_line
         set $_variable ""
     end
-    echo " = = = "
     while read _line
-        echo 3 $_line
         for _variable in $_variables
-            echo 4 $_variable
             set $_options $_variable $$_variable$_line\n
         end
     end
