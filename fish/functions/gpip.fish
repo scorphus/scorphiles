a5415384ae8b6f28e7f47d9f1ae17450a86af952
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-18 19:11:12 -0300

    refact(fish): merge gpip and gpip15 functions into one

--- fish/functions/gpip.fish
+++ fish/functions/gpip.fish
@@ -1,18 +1,23 @@
 function gpip -d "Globoi's artifactory-aware pip"
     if test (count $argv) -ge 2
         if test $argv[1] = 'install'
+            set trusted_host_a "--trusted-host" "pypi.globoi.com"
+            set trusted_host_b "--trusted-host" "artifactory.globoi.com"
+            pip --version | awk '{print $2}' | read -l pip_version
+            if [ "$pip_version" = "1.5.6" ]
+                set -e trusted_host_a
+                set -e trusted_host_b
+            end
             pip install \
                 --allow-external \
                 --allow-unverified \
                 --process-dependency-links \
-                --trusted-host pypi.globoi.com \
-                --trusted-host artifactory.globoi.com \
                 --index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi/simple \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi/simple/ \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/pypi/pypi-all/simple/ \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/ipypi/pypi/simple/ \
                 --extra-index-url=https://artifactory.globoi.com/artifactory/api/ipypi/pypi-all/simple/ \
-                -U $argv[2..-1]
+                $trusted_host_a $trusted_host_b -U $argv[2..-1]
         else
             echo 'Sorry, we only support `install`, mate :('
             return 1
