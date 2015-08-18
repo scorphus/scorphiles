438c309d5fe45df1bb5ee31da5fe91278adab700
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-18 19:12:43 -0300

    refact(fish): update fuck completions function

--- fish/completions/fuck.fish
+++ fish/completions/fuck.fish
@@ -1,10 +1,12 @@
 # TheFuck by way of changing the command line
 
 function __fuck_complete
+    set -x TF_ALIAS fuck
+    set -x THEFUCK_REQUIRE_CONFIRMATION False
     set -l fucked_up_command $history[1]
-    set -l unfucked (thefuck $fucked_up_command ^/dev/null)
-    if [ "$unfucked" != "" ]
-        commandline -r $unfucked
+    thefuck $fucked_up_command | read unfucked_command
+    if [ "$unfucked_command" != "" ]
+        commandline -r $unfucked_command
     else
         commandline -r ""
     end
