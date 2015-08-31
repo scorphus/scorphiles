07f2c6ed0b2668e78cadae452e5317ae3cd46a9c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-08-31 15:23:54 -0300

    improve(fish): add path to z in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,5 +9,7 @@ set -g OMF_CONFIG $HOME/.config/omf
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
 # to create a custom plugin instead.
 
+set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
+
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
