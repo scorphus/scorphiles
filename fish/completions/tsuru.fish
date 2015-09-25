d9d1a4623e135b61230ce44e1cbce759210f40e4
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-25 19:10:58 -0300

    cleanup(completions.tsuru): remove a useless `echo`

--- fish/completions/tsuru.fish
+++ fish/completions/tsuru.fish
@@ -5,7 +5,6 @@ function __add_tsuru_completions -d 'Add Tsuru completions'
         set command_text (expr $arg : '.*\|\(.*\)')
         if test "$command_text" != ''
             set command_name (expr $arg : '\(.*\)\|.*')
-            echo "complete -c tsuru -a $command_name -d \"$command_text\""
             complete -c tsuru -a $command_name -d "$command_text"
         end
     end
