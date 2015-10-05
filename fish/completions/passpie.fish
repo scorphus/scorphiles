74d71480216742b9995a7e36eff6d3fe1df27ca1
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-05 11:02:45 -0300

    improve(fish): update passpie completions function

--- fish/completions/passpie.fish
+++ fish/completions/passpie.fish
@@ -1,10 +1,14 @@
 # passpie completions for Fish Shell
 
-function __fish_passpie_credentials
-  grep -Ehrio '[A-Z0-9._%+-]+@[A-Z0-9.-]+(@[A-Z0-9_\-\.]+)?' /Users/pablo/.passpie
+function _passpie_credentials
+  grep -EhriIo '[A-Z0-9._%+-]+@[A-Z0-9.-]+(@[A-Z0-9_\-\.]+)?' /Users/pablo/.passpie
 end
 
-function __fish_passpie_needs_command
+function _passpie_credential_names
+  ls -1 /Users/pablo/.passpie
+end
+
+function _passpie_needs_command
   set cmd (commandline -opc)
   if [ (count $cmd) -eq 1 -a $cmd[1] = 'passpie' ]
     return 0
@@ -12,7 +16,7 @@ function __fish_passpie_needs_command
   return 1
 end
 
-function __fish_passpie_using_command
+function _passpie_using_command
   set cmd (commandline -opc)
   if [ (count $cmd) -gt 1 ]
     for arg in $argv
@@ -24,6 +28,7 @@ function __fish_passpie_using_command
   return 1
 end
 
-complete -f -c passpie -n '__fish_passpie_needs_command' -a 'add copy remove update' --description 'Manage a credential'
-complete -f -c passpie -n '__fish_passpie_using_command add copy remove update' -a '(__fish_passpie_credentials)' --description 'Credential'
+complete -f -c passpie -n '_passpie_needs_command' -a 'add copy remove search update' --description 'Manage a credential'
+complete -f -c passpie -n '_passpie_using_command add copy remove search update' -a '(_passpie_credentials)
+                                                                  (_passpie_credential_names)' --description 'Credential'
 
