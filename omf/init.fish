35e023cdcaed0a3e8cf098b861577906f0baa2b3
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-15 16:18:41 -0300

    feat(omf): add Oh-My-Fish dotfiles back in the game

---
+++ omf/init.fish
@@ -0,0 +1,17 @@
+# virtualfish
+set PROJECT_HOME $HOME/Workspace
+set VIRTUALFISH_COMPAT_ALIASES
+eval (python -m virtualfish auto_activation compat_aliases global_requirements projects)
+if test "$VIRTUAL_ENV" != ""
+    vf activate (basename $VIRTUAL_ENV)
+end
+
+# golang
+set -x GOPATH $HOME/gocode
+set -x PATH $GOPATH/bin $PATH
+
+# general purpose scripts
+set -x PATH $HOME/bin $PATH
+
+# scorphish theme
+set -g SCORPHISH_GIT_INFO_ON_FIRST_LINE
