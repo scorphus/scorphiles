9d976d40950fde4e562cbb750527b0b287f9cf98
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-11-06 11:19:31 -0300

    feat(fish): add three more key binds, thefuck and copy/paste

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
 function fish_user_key_bindings -d 'bind peco_select_history to ctrl+r'
   bind \cr 'peco_select_history (commandline -b)'
+  bind \e\e 'fuck-command-line'
+  bind \[1\;5A 'echo -n $history[1] | pbcopy'
+  bind \[1\;5B 'commandline -r (pbpaste)'
 end
