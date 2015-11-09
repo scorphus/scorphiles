483dae5af89e5a6232ae5162999c428d4f2a7a50
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-11-09 17:15:12 -0200

    refact(fish_user_key_bindings.\e\e): use the up-to-date function name

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
-  bind \e\e 'fuck-command-line'
+  bind \e\e 'thefuck-command-line'
   bind \[1\;5A 'echo -n $history[1] | pbcopy'
   bind \[1\;5B 'commandline -r (pbpaste)'
 end
