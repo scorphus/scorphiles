54513d9c95758f477abe543d86fa48c13bc3e399
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-11-09 17:16:56 -0200

    feat(fish_user_key_bindings): bind Ctrl+F to thefuck

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \e\e 'thefuck-command-line'
+  bind \cf 'thefuck-command-line'
   bind \[1\;5A 'echo -n $history[1] | pbcopy'
   bind \[1\;5B 'commandline -r (pbpaste)'
 end
