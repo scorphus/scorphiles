835184960ce9e6c6608c380b037422b3720f965d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-11-22 22:52:37 -0200

    feat(fish): add a new g alias, for git

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,5 @@
 alias clean-pycs='find . -name "*.pyc" -delete'
+alias g='git'
 alias ls-untracked='git clean -fd --dry-run | sed s/^Would\ remove\ //'
 alias lszip='unzip -l $zip | less -XRF'
 alias mcd='mkdir $argv; and cd $argv'
