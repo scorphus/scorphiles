cd777ecbf95ab5471dc69fbbbf0213dbabb668bd
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-06-13 23:18:50 -0300

    tar-gzip-encrypt: fix $argv indexing

--- fish/functions/tar-gzip-encrypt.fish
+++ fish/functions/tar-gzip-encrypt.fish
@@ -3,5 +3,5 @@ function tar-gzip-encrypt -d "Compress w/ tar and gzip and encrypt w/ openssl"
     echo "Usage: tar-gzip-encrypt <compressed_name> <file[ file ...]>"
     return 0
   end
-  tar cz $argv[2:-1] | openssl enc -aes-256-cbc -e > $argv[1].tar.gz.enc
+  tar cz $argv[2..-1] | openssl enc -aes-256-cbc -e > $argv[1].tar.gz.enc
 end
