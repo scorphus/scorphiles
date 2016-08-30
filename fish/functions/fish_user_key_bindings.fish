025c65d5eb445ae62c6a5d46e327c938c19f28f2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-08-29 23:49:05 -0300

    fish_user_key_bindings: copy current commandline on ctrl+down-arrow

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,5 +3,5 @@ function fish_user_key_bindings
   bind \e\e 'thefuck-command-line'
   bind \cf 'thefuck-command-line'
   bind \[1\;5A 'echo -n $history[1] | pbcopy'
-  bind \[1\;5B 'commandline -r (pbpaste)'
+  bind \[1\;5B 'echo (commandline) | tr -d "\n" | pbcopy'
 end
