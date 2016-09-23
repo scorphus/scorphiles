642aea46fa7df7c32ac7dd08c6fb4b5c5884648e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-09-23 20:50:48 -0300

    omf/init: set scorphish theme pormpt variables

--- omf/init.fish
+++ omf/init.fish
@@ -12,6 +12,8 @@ end
 
 # scorphish theme
 set -g SCORPHISH_GIT_INFO_ON_FIRST_LINE
+set -g theme_display_rust yes
+set -g theme_display_nvm yes
 
 # set sensitive variables
 source $OMF_CONFIG/sensitive.fish ^/dev/null
