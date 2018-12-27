a8ff865ecd7d28cf8d37a1733071b658a2213773
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-12-27 12:29:38 +0100

    omf/init: remove virtualfish stuff

--- omf/init.fish
+++ omf/init.fish
@@ -1,14 +1,4 @@
 # virtualfish
-set -g PROJECT_HOME $HOME/Workspace
-set -g VIRTUALFISH_COMPAT_ALIASES
-set -l vf_plugins auto_activation compat_aliases global_requirements projects
-set -l vf_loader (python -m virtualfish $vf_plugins ^ /dev/null)
-if test -n "$vf_loader"
-    eval $vf_loader
-    if test "$VIRTUAL_ENV" != ""; and type -q vf
-        vf activate (basename $VIRTUAL_ENV) 2>&1 > /dev/null
-    end
-end
 
 # scorphish theme
 set -g SCORPHISH_GIT_INFO_ON_FIRST_LINE
