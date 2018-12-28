bb89fb30fdf351504e4b86090256dacd0bcf26a9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-12-28 22:09:20 +0100

    fish/config: move omf stuff into conf.d

--- fish/config.fish
+++ fish/config.fish
@@ -18,25 +18,11 @@ set -xg PYTHONDONTWRITEBYTECODE 1  # Don't write bytecode files everywhere
 # TheFuck stuff
 set -xg TF_OVERRIDDEN_ALIASES 'cd,grep,ls,man,open,git'
 
-# Path to your oh-my-fish.
-set -g OMF_PATH $HOME/.local/share/omf
-
-# Path to your oh-my-fish configuration.
-set -g OMF_CONFIG $HOME/.config/omf
-
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish
-# is loaded. For common configurations, we advise you to add them to your
-# $OMF_CONFIG/init.fish file or to create a custom plugin instead.
-
 # z omf pkg config
 set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
 
 # gem omf pkg config
 set -x GEM_ROOT $HOME/.gem
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
 
 # Load aliases
 source $HOME/.config/fish/aliases.fish
