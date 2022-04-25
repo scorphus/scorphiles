0788188038550f5fd4925aced2ec032ad310447f
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-04-25 20:30:43 +0200

    fish/config.fish: Run only on interactive mode

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -5,3 +5,10 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
+
+# scorphish theme options
+set -g theme_display_ruby yes
+set -g theme_display_rust yes
+set -g theme_display_node yes
+set -g theme_display_pwd_on_second_line yes
+set -g theme_display_git_on_first_line yes
--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,26 @@
-# path
-set -gx PATH $HOME/.local/bin $PATH /usr/local/sbin
+if status is-interactive
+    # path
+    set -gx PATH $HOME/.local/bin $PATH /usr/local/sbin
 
-# golang
-set -gx GOPATH $HOME/gocode
-set -gx GOBIN $GOPATH/bin
-set -gx PATH $GOBIN $PATH
+    # golang
+    set -gx GOPATH $HOME/gocode
+    set -gx GOBIN $GOPATH/bin
+    set -gx PATH $GOBIN $PATH
 
-# general purpose scripts
-for dir in (find -H $HOME/bin -maxdepth 1 -type d)
-    set -x PATH $dir $PATH
+    # general purpose scripts
+    for dir in (find -H $HOME/bin -maxdepth 1 -type d | sort)
+        set -x PATH $dir $PATH
+    end
+
+    # Python stuff
+    set -xg PYTHONDONTWRITEBYTECODE 1  # Don't write bytecode files everywhere
+
+    # TheFuck stuff
+    set -xg THEFUCK_OVERRIDDEN_ALIASES 'aws,cd,git,grep,ls,man,open,pyenv'
+
+    # z omf pkg config
+    set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
+
+    # gem omf pkg config
+    set -x GEM_ROOT $HOME/.gem
 end
-
-# Python stuff
-set -xg PYTHONDONTWRITEBYTECODE 1  # Don't write bytecode files everywhere
-
-# TheFuck stuff
-set -xg THEFUCK_OVERRIDDEN_ALIASES 'aws,cd,git,grep,ls,man,open,pyenv'
-
-# z omf pkg config
-set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
-
-# gem omf pkg config
-set -x GEM_ROOT $HOME/.gem
