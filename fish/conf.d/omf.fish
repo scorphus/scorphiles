852ecade40100cfc1d556d269bdc8326cfa63630
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2024-11-21 10:44:42 +0100

    fish/conf.d/omf: Set scorphish before initiating OMF

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -1,3 +1,10 @@
+# scorphish theme options
+set -g theme_display_ruby yes
+set -g theme_display_rust yes
+set -g theme_display_node yes
+set -g theme_display_pwd_on_second_line yes
+set -g theme_display_git_on_first_line yes
+
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
@@ -5,10 +12,3 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-
-# scorphish theme options
-set -g theme_display_ruby yes
-set -g theme_display_rust yes
-set -g theme_display_node yes
-set -g theme_display_pwd_on_second_line yes
-set -g theme_display_git_on_first_line yes
